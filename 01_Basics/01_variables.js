7a81a6158f58d517f6ba6880c399fe9e52382264
uzairit92 <52896014+uzairit92@users.noreply.github.com>
2025-05-23 05:44:25 +0000

    new change

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accoutnId = 2
+
+accountEmail = "hc@hc"
+accountPassword = "2431432134"
+accountCity = "Lahore"
+
+console.log(accountId);
+console.log([accountId, accountEmail, accountPassword, accountCity])
+
+/*
+Prefer not to use var
+becuase of issue in block scope and functional scope 
+*/
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all  js Code as newer version 
+
+
+// alert("hello");
+
+// alert(3+3) // we are using node js not browser
+
+
+console.log(3+3);
+
+console.log("Hitesh");
+
+// code read abality 
+
+// ECMA Script 2023 Language Specification 
+
+
+// 
